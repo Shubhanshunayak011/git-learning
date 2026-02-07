d68a3cebeab2ff9544298291f6ce21946a885228
Shubhanshunayak011 <nayakshubhanshu9@gmail.com>
2026-02-07 17:16:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Header />
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route path="service" element={<Service />} />
           <Route path="contact" element={<Contact />} />
 
